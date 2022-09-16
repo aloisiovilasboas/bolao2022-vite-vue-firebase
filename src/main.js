ec7a1f690d0548893b22fd1d3dcb856542b8b1b6
Aloisio <aloisiovilasboas@gmail.com>
2022-09-16 13:48:38 -0300

    faxinando

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
+
 import './style.css'
 import App from './App.vue'
 import router from "./router"
-import "./index.css";
+
+// Import our custom CSS
+//import '../src/scss/styles.scss'
+
+// Import all of Bootstrap's JS
+// import bootstrap from 'bootstrap'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
