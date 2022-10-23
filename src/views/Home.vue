0e285d33f6f196986c251be96982e963bec9b72e
Aloisio <aloisiovilasboas@gmail.com>
2022-10-23 00:58:20 -0300

    plis play

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
                 Bem vindo
             </template>
             <template #content>
-                <p>Para participar do Bolão do AFC 2022, entre em contato com algum membro da organização. Em seguida, clique no link fornecido pela organização e realize o seu cadastro. Atenção, o link é individual e intransferível e funciona para um único cadastro. Preencha os dados das suas apostas e submeta os seus palpites até as 12h do dia 14 de junho de 2018, quando será dado o pontapé inicial da Copa do Mundo FIFA 2018. Lembre que para garantir a sua inscrição, é preciso também, até a mesma data e horário, confirmar o pagamento da sua cota de R$25,00 a um dos organizadores. Não serão aceitos palpites submetidos após o prazo. Depois disso, é só aguardar os resultados dos jogos e torcer pelas seleções que você apostou.</p>
+                <p>Plis play</p>
                 <p>Deseja se inscrever no bolão?</p>
                 
             </template>
